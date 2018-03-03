6cee0c72bac2239d44f9a452bdb2849af63d3252
João Luiz Schlickmann Kestering <jlsk25@gmail.com>
2018-03-03 14:53:27 -0300

    Eureka Service Discovery

--- cloud-lab01/src/main/java/cloudlab01/cloudlab01/CloudLab01Application.java
+++ cloud-lab01/src/main/java/cloudlab01/cloudlab01/CloudLab01Application.java
@@ -2,11 +2,13 @@ package cloudlab01.cloudlab01;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CloudLab01Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CloudLab01Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CloudLab01Application.class, args);
+    }
 }
