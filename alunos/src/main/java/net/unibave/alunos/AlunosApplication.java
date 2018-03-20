fb189fa895b235f368b3f0d77e3f720a6421849b
João Luiz Schlickmann Kestering <jlsk25@gmail.com>
2018-03-19 23:25:42 -0300

    hystrix dashboard and turbine stream

--- alunos/src/main/java/net/unibave/alunos/AlunosApplication.java
+++ alunos/src/main/java/net/unibave/alunos/AlunosApplication.java
@@ -2,15 +2,17 @@ package net.unibave.alunos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableFeignClients
+@EnableCircuitBreaker
 public class AlunosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlunosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AlunosApplication.class, args);
+    }
 }
