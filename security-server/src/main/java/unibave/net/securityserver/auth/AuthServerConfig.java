7aa99662d5dc36b134b6786d518221b00586d190
João Luiz Schlickmann Kestering <jlsk25@gmail.com>
2018-04-23 21:44:47 -0300

    Add JWT in security server

--- security-server/src/main/java/unibave/net/securityserver/auth/AuthServerConfig.java
+++ security-server/src/main/java/unibave/net/securityserver/auth/AuthServerConfig.java
@@ -9,6 +9,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.A
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenEnhancer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
 
 @Configuration
 @EnableAuthorizationServer
@@ -17,6 +20,15 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
     @Autowired
     @Qualifier("authenticationManagerBean")
     AuthenticationManager authenticationManager;
+
+    @Autowired
+    TokenStore tokenStore;
+
+    @Autowired
+    JwtAccessTokenConverter accessTokenConverter;
+
+    @Autowired
+    TokenEnhancer tokenEnhancer;
 
 
     @Override
@@ -55,6 +67,9 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints.tokenStore(tokenStore)
+                .accessTokenConverter(accessTokenConverter)
+                .tokenEnhancer(tokenEnhancer)
+                .authenticationManager(authenticationManager);
     }
 }
